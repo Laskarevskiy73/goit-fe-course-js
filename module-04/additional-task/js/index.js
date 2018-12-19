e140659ebd092834e2ee93854b945ab91f82697d
laskarevskiy72 <laskarevskiy73@gmail.com>
2018-12-20 00:20:30 +0200

    add additional tasks

--- module-04/additional-task/js/index.js
+++ module-04/additional-task/js/index.js
@@ -92,12 +92,200 @@
 
 */
 
-const tasksCompleted = {
-    ann: 29,
-    david: 35,
-    helen: 1,
-    lorence: 99
-};
+// const tasksCompleted = {
+//   ann: 29,
+//   david: 35,
+//   helen: 1,
+//   lorence: 99
+// };
 
+// const values = Object.values(tasksCompleted);
+// let maxValue = values[0];
+
+
+// for (let value of values) {
+
+//   if (value > maxValue) {
+//     maxValue = value;
+//   }
+// }
+
+// for (let value in tasksCompleted) {
+
+//   if (tasksCompleted[value] === maxValue) {
+//     console.log(`Maximum number of tasks made : ${value} - ${tasksCompleted[value]}`);
+//   }
+// }
+// console.log(tasksCompleted);
+
+//=============================================================================
+
+
+/*
+  task-03
+
+  Напишите функцию countProps(obj),
+  считающую кол-во свойств в объекте.
+  Функция возвращает количество свойств.
+
+*/
+
+// const countProps = obj => {
+
+//   const keys = Object.entries(obj);
+
+//   return keys.length;
+
+// }
+
+// // Вызовы функции для проверки
+// console.log(
+//   countProps({})
+// ); // 0
+
+// console.log(
+//   countProps({
+//     a: 1,
+//     b: 3,
+//     c: 'hello'
+//   })
+// ); // 3
+
+//=============================================================================
+
+
+/*
+  task-04
+
+  Создайте функцию isObjectEmpty(obj), которая получает 
+  один аргумент obj - объект, и проверяет пуст ли он (есть ли в нем свойства).
+  
+  Возвращает true если объект пустой, false если не пустой.
+
+*/
+
+// const isObjectEmpty = obj => {
+
+//   const values = Object.entries(obj);
+//   const resultObj = values.length === 0;
+
+//   return resultObj;
+// }
+
+// // Вызовы функции для проверки
+// console.log(
+//   isObjectEmpty({})
+// ); // true
+
+// console.log(
+//   isObjectEmpty({
+//     a: 1
+//   })
+// ); // false
+
+// console.log(
+//   isObjectEmpty({
+//     a: 1,
+//     b: 2
+//   })
+// ); // false
+
+//=============================================================================
+
+
+/*
+  task-05
+
+  Напишите функцию countTotalSalary(salaries),
+  получающую объект и считающую общую сумму запрплаты работников.
+  
+  Каждое поле объекта передаваемого в функцию, имеет вид "имя":"зарплата"
+  
+  Функция возвращает общую сумму зарплаты.
+
+*/
+
+// const countTotalSalary = salaries => {
+
+//   let total = 0;
+//   const valuesSalaries = Object.values(salaries);
+
+//   for (let value of valuesSalaries) {
+
+//     total += value;
+//   }
+
+//   return total;
+
+// }
+
+// // Вызовы функции для проверки
+// console.log(
+//   countTotalSalary({})
+// ); // 0
+
+// console.log(
+//   countTotalSalary({
+//     mango: 100,
+//     poly: 150,
+//     alfred: 80
+//   })
+// ); // 330
+
+//=============================================================================
+
+
+/*
+  task-06
+
+Напишите функцию getAllPropValues(arr, prop), 
+  которая получает массив объектов и имя ключа, 
+  возвращает массив значений определенного поля prop
+  из каждого объекта в массиве
+
+*/
+
+const getAllPropValues = (arr, prop) => {
+
+  // console.log(arr);
+
+  for (let i of arr) {
+
+    console.log(i[prop]);
+
+  }
+
+
+}
+
+const users = [{
+    name: 'Poly',
+    age: 7,
+    mood: 'happy'
+  },
+  {
+    name: 'Mango',
+    age: 4,
+    mood: 'blissful'
+  },
+  {
+    name: 'Ajax',
+    age: 3,
+    mood: 'tired'
+  }
+];
+
+// Вызовы функции для проверки
+console.log(
+  getAllPropValues(users, 'name')
+); // ['Poly', 'Mango', 'Ajax']
+
+console.log(
+  getAllPropValues(users, 'mood')
+); // ['happy', 'blissful', 'tired']
+
+console.log(
+  getAllPropValues(users, 'active')
+); // []
 
 //=============================================================================
